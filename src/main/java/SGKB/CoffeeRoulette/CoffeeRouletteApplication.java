c5e000ad0aefe29c27e7bb0ea56294b04e04047b
Lewin <Lewin.rutz@gmail.com>
2022-06-20 16:55:04 +0200

    added maven and trying to initialise spring boot

---
+++ src/main/java/SGKB/CoffeeRoulette/CoffeeRouletteApplication.java
@@ -0,0 +1,13 @@
+package SGKB.CoffeeRoulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeRouletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SGKB/CoffeeRoulette/CoffeeRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package SGKB.CoffeeRoulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
