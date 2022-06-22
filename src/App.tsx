4abed030dac48b2b42e0d321f4bafb2d1d377efd
Lewin <Lewin.rutz@gmail.com>
2022-06-22 09:33:37 +0200

    added sso to login, but still singlepage, until now

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,52 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logoCoffeeRoulette.svg';
 import './App.css';
-import { NavLink } from 'react-router-dom';
-import { Routes ,Route } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import {useState} from "react";
+import {GoogleLogin} from "react-google-login";
 
 const App = () => (
     <div className='app'>
-      <h1>React Router Demo</h1>
-      <Navigation />
-      <Main />
+        <Login/>
     </div>
 );
 
-let activeStyle = {
-    textDecoration: "underline",
-};
+const clientID = "231963202217-njj9ka9ivkh29p9k1bmmjojcpi171ing.apps.googleusercontent.com";
 
-const Navigation = () => (
-    <nav>
-      <ul>
-        <li><NavLink to='/' >Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/contact'>Contact</NavLink></li>
-      </ul>
-    </nav>
-);
+function Login() {
+    const onSuccess = (res: any) =>{
+        console.log('[Login Success] currentUser: ', res.profileObj);
+    };
+    const onFailure = (res: { profileObj: any; }) =>{
+        console.log('[Login failure] res: ', res);
+    };
 
-const Main = () => (
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/about' element={<Home/>}></Route>
-      <Route path='/contact' element={<Home/>}></Route>
-    </Routes>
-);
+    return (
+        <div className="flex-box align-items-center justify-content-center">
+            <div className="text-center">
+                <img className={"img-logo"} src={logo} alt="Logo Coffee Roulette" />
+                <h2>Login</h2>
+                <GoogleLogin
+                    clientId={clientID}
+                    buttonText="Login"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    style={{marginTop: "100px"}}
+                    isSignedIn={true}
+                />
+            </div>
+        </div>
+    );
 
-const Home = () => (
-    <div className='home'>
-      <h1>Welcome to my portfolio website</h1>
-      <p> Feel free to browse around and learn more about me.</p>
-    </div>
-);
-
-const About = () => (
-    <div className='about'>
-      <h1>About Me</h1>
-      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    </div>
-);
-
-const Contact = () => (
-    <div className='contact'>
-      <h1>Contact Me</h1>
-      <p>You can reach me via email: <strong>hello@example.com</strong></p>
-    </div>
-);
+    function onSignIn(googleUser: { getBasicProfile: () => any; }) {
+        let profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+}
 
 export default App;
+
