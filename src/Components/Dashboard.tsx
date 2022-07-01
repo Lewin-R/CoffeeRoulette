79115bbdc50fa776e8add3c2cef9a98dc9e18686
Lewin-R <lewin.rutz@gmail.com>
2022-07-01 20:01:03 +0200

    khjsdf

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import Navbar from './Navbar';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { DateRangePicker} from 'react-dates';
+import "react-dates/lib/css/_datepicker.css";
 
 const Dashboard = () => {
-
-        return (
+    const [startDate, setStartDate] = React.useState();
+    const [endDate, setEndDate] = React.useState();
+    const [focusedInput, setFocusedInput] = React.useState();
+    return (
+        <div>
             <Navbar/>
-        );
+            <div className={""}>
+                <div className="float-container">
+
+                    <div className="App">
+                        <DateRangePicker
+                            startDate={startDate}
+                            startDateId="start-date"
+                            endDate={endDate}
+                            endDateId="end-date"
+                            onDatesChange={({startDate, endDate}) => {
+                                setStartDate(startDate);
+                                setEndDate(endDate);
+                            }}
+                            focusedInput={this.state.focusedInput}
+                            onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
+                        />
+                    </div>
+                </div>
+
+                <div className="float-child">
+                    <label htmlFor="">Bis:</label>
+                    <br/>
+
+                </div>
+            </div>
+        </div>
+
+)
+    ;
 }
 
 export default Dashboard;
